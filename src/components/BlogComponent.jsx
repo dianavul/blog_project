9a1dfe5b958cedca68a83e8cf0df52f24a28412c
Diana Vulturar <diana_vulturar@yahoo.com>
2023-03-13 19:11:12 +0100

    WishList

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -1,5 +1,23 @@
 import { Card, CardBody, CardTitle, CardText, Button, Col } from "reactstrap";
 function BlogComponent({ blog }) {
+	const addToWishList = () => {
+		const blogsString = localStorage.getItem("bloguri");
+		if (blogsString !== null) {
+			const blogs = JSON.parse(blogsString);
+			const existNr = blogs.find((blogElement) => {
+				return blogElement.id === blog.id;
+			});
+
+			if (existNr === undefined) {
+				blogs.push(blog);
+			}
+			localStorage.setItem("bloguri", JSON.stringify(blogs));
+		} else {
+			const newBlogs = [];
+			newBlogs.push(blog);
+			localStorage.setItem("bloguri", JSON.stringify(newBlogs));
+		}
+	};
 	return (
 		<Col className='mt-4'>
 			<Card>
@@ -12,7 +30,12 @@ function BlogComponent({ blog }) {
 
 					<CardText>{blog.body}</CardText>
 					<h4>{blog.userId}</h4>
-					<Button>Add to favourite</Button>
+					<Button
+						onClick={() => {
+							addToWishList();
+						}}>
+						Add to favourite
+					</Button>
 				</CardBody>
 			</Card>
 		</Col>
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,4 +1,43 @@
+import { useState, useEffect } from "react";
+import { Button } from "reactstrap";
+
 function Wishlist() {
-	return <h1>Wish List</h1>;
+	//2.creez variabila de stare pentru date
+	const [wishListBlogs, setWishListBlogs] = useState([]);
+	//1.preluare date din Local Storage
+	useEffect(() => {
+		const blogsString = localStorage.getItem("bloguri");
+		if (blogsString !== null) {
+			const blogs = JSON.parse(blogsString);
+			setWishListBlogs(blogs);
+		}
+	}, []);
+	const onDelete = (blogId) => {
+		const filteredBlogs = wishListBlogs.filter((blog) => blog.id !== blogId);
+		localStorage.setItem("bloguri", JSON.stringify(filteredBlogs));
+		setWishListBlogs(filteredBlogs);
+	};
+	return (
+		<>
+			<h2>Wishlist</h2>
+			<ul>
+				{wishListBlogs.map((blog, index) => {
+					return (
+						<li key={index}>
+							{blog.title}
+							<Button
+								color='danger'
+								className='mt-2 ms-4'
+								onClick={() => {
+									onDelete(blog.id);
+								}}>
+								DELETE
+							</Button>
+						</li>
+					);
+				})}
+			</ul>
+		</>
+	);
 }
 export default Wishlist;
